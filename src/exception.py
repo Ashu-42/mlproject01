6a5d65abde8384b055b125f67480a77ed271a8d4
Ashutosh Deb <namrataashutosh42@gmail.com>
2026-05-17 12:58:30 +0530

    exception and logging

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+# common for every project
+
+import sys
+from src.logger import logging
+
+def error_message_details(error, error_detail:sys):
+
+    _,_, exc_tb = error_detail.exc_info() # gives all the info related to error, not interested in the first 2 values, the 3rd value gives data like file in which exception occured, line of error, error type etc 
+    
+    err_file, err_line,  err_msg = exc_tb.tb_frame.f_code.co_filename, exc_tb.tb_lineno, str(error)
+    
+    error_message = f"error occured in python script name : {err_file}, line num : {err_line}, error message : {err_msg}"
+
+    return error_message
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error=error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("division by zero")
+        raise CustomException(e, sys)
---
+++ src/logger.py
@@ -0,0 +1,25 @@
+# common for every project
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" #names the log file with the timestamp  of execution
+
+# LOG PATH
+logs_dir=os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True) # exist_ok=True -> even if file exists in the directory just go and append the new file
+
+# LOG FILE PATH
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# to overwrite the functionality of normal logging, we have to update this in the basic config
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+if __name__=="__main__":
+    logging.info("logging has started")
